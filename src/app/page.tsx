c1104aa0fc3910bea1f9d297223591dbc14986d1
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-02-09 20:21:39 +0330

    [add] banner image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
 import { type ReactNode } from "react";
+import styles from "./home.module.css";
+import Image from "next/image";
+import bnannerImage from "@/assets/images/banner.jpg";
 
 export default function Home(): ReactNode {
-  return <div>Home</div>;
+  return (
+    <div className={styles.home}>
+      <div className={styles.banner}>
+        <section className={styles["banner-image-wrapper-1"]}>
+          <Image
+            src={bnannerImage}
+            alt=""
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </section>
+      </div>
+    </div>
+  );
 }
