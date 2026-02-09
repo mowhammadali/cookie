10ecb1ad9ecbad4b48251aeb62574883535f7ba7
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-02-09 15:42:21 +0330

    [add] sidebar logo

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -1,6 +1,24 @@
 import { type ReactNode } from "react";
 import styles from "./sidebar.module.css";
+import Image from "next/image";
+import logoImage from "@/assets/images/cookie-32x32.png";
+import MingcuteLeftLine from "@/icons/MingcuteLeftLine";
 
 export default function Sidebar(): ReactNode {
-  return <div className={styles.sidebar}>sidebar</div>;
+  return (
+    <div className={styles.sidebar}>
+      <header className={styles.header}>
+        <section className={styles["logo-section"]}>
+          <p>Cookie</p>
+          <Image src={logoImage} alt="logo" />
+        </section>
+        <section className={styles["left-icon-section"]}>
+          <MingcuteLeftLine
+            color="var(--text-tertiary)"
+            className={styles["left-icon"]}
+          />
+        </section>
+      </header>
+    </div>
+  );
 }
