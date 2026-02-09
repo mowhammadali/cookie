02654f3b6ca1fd73bc37e15133cd73aafb458491
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-02-09 12:48:04 +0330

    [setup] pre commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { type ReactNode } from "react";
 
-export default function Home(): ReactNode {
+export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
