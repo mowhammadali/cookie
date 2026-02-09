fe1e4b5fae7f7544ff7ceeb0fcc024528efe4329
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-02-09 14:15:20 +0330

    [setup] global colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/colors.css";
 import "./globals.css";
 import { type ReactNode } from "react";
 
